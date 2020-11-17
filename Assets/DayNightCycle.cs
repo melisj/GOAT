15d3d9d06463a94b2cbbc288c4889004dac163a1
Joppe Min <joppemin@hotmail.com>
2020-11-17 16:26:59 +0100

    Initial timecycle scene and script. M1 to switch Day/Night

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    private Light mainLight;
+
+    [SerializeField]
+    private Color dayTimeColor = new Color(1,1,1);
+    [SerializeField]
+    private Color nightTimeColor = new Color(0,0,0);
+
+    private Color currentTimeColor;
+    private Color targetTimeColor;
+
+    private bool isDay;
+    private float TransitionTimer;
+
+    void Start()
+    {
+        mainLight = this.gameObject.GetComponent<Light>();
+
+        setTimeDay();
+    }
+
+    void Update()
+    {
+        TransitionTimer += Time.deltaTime;
+        mainLight.color = Color.Lerp(currentTimeColor, targetTimeColor, TransitionTimer);
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isDay)
+            {
+                setTimeNight();
+            }
+            else
+            {
+                setTimeDay();
+            }
+            
+        }
+    }
+
+    public void setTimeDay()
+    {
+        currentTimeColor = dayTimeColor;
+        targetTimeColor = nightTimeColor;
+
+        isDay = true;
+        TransitionTimer = 0f;
+    }
+
+    public void setTimeNight()
+    {
+        currentTimeColor = nightTimeColor;
+        targetTimeColor = dayTimeColor;
+
+        isDay = false;
+        TransitionTimer = 0f;
+    }
+}
