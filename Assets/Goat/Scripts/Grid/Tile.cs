7a04ec5319731c495a8b4ef043242c38d988bebd
jasperbarg <jaspervandenbarg@hotmail.nl>
2020-11-19 11:04:10 +0100

    added TileAssets to find assets to place on tiles in the resources folder.

--- Assets/Goat/Scripts/Grid/Tile.cs
+++ Assets/Goat/Scripts/Grid/Tile.cs
@@ -3,19 +3,30 @@ using UnityEngine;
 
 namespace GOAT.Grid
 {
-    public enum TileType
+    public enum BuildingType
     {
         Empty,
         Building
     }
 
+    public enum FloorType
+    {
+        Empty,
+        Wood,
+        Glass,
+        Steel
+    }
+
     public class Tile
     {
         private Vector3 centerPosition;
-        private bool isSolid;
+        private bool isFloor;
         private bool isUnlocked;
-        private bool isSelected;
-        private TileType tileType;
+        private BuildingType buildingType;
+        private FloorType floorType;
+        private GameObject floorObject, buildingObject;
+
+        public bool IsFloor { get => isFloor; }
 
         public Tile(Vector3 centerPosition) {
             this.centerPosition = centerPosition;
@@ -32,19 +43,35 @@ namespace GOAT.Grid
         public TileInformation GetTileInformation()
         {
             // Initialize tile information
-            return new TileInformation(centerPosition, tileType);
+            return new TileInformation(centerPosition, buildingType);
+        }
+
+        public void EditFloor(FloorType floorType)
+        {
+            MonoBehaviour.Destroy(floorObject);
+            //floorObject = GameObject.Instantiate(newObject, centerPosition, Quaternion.identity);
+
+            //isFloor = newObject != null;
+        }
+
+        public void EditBuilding(BuildingType buildingType)
+        {
+            MonoBehaviour.Destroy(buildingObject);
+
+            //buildingObject = MonoBehaviour.Instantiate(newObject, centerPosition, Quaternion.identity);
+            this.buildingType = buildingType;
         }
     }
 
     public struct TileInformation
     {
         public readonly Vector3 TilePosition;
-        public readonly TileType TileType;
+        public readonly BuildingType buildingType;
 
-        public TileInformation(Vector3 TilePosition, TileType TileType)
+        public TileInformation(Vector3 TilePosition, BuildingType buildingType)
         {
             this.TilePosition = TilePosition;
-            this.TileType = TileType;
+            this.buildingType = buildingType;
         }
     }
 }
---
+++ Assets/Goat/Scripts/Grid/TileAssets.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GOAT.Grid {
+    public static class TileAssets
+    {
+        private static Dictionary<Enum, GameObject> tileAssets = new Dictionary<Enum, GameObject>() { };
+        //private static Dictionary<BuildingType, GameObject> buildingAssets = new Dictionary<BuildingType, GameObject>();
+
+        public static void InitializeAssetsDictionary() {
+            tileAssets.Add(FloorType.Empty, new GameObject());
+            
+            //UnityEngine.Resources.Load("");
+        }
+
+        public static GameObject FindAsset(Enum enumValue) {
+            return tileAssets[enumValue];
+        }
+    }
+}
