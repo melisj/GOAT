dd4dad16b47858196f74cd4351da5a80e6e02799
Joppe Min <joppemin@hotmail.com>
2020-11-19 14:02:03 +0100

    Day night cycle on a time based clock

--- Assets/DayNightCycle.cs
+++ Assets/DayNightCycle.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayNightCycle : MonoBehaviour
 {
     private Light mainLight;
+    public Text timeText;
 
     [SerializeField]
     private Color dayTimeColor = new Color(1,1,1);
@@ -14,51 +16,84 @@ public class DayNightCycle : MonoBehaviour
     private Color currentTimeColor;
     private Color targetTimeColor;
 
+    //transition timer for lerping the time of day
+    private float transitionTimer;
     private bool isDay;
-    private float TransitionTimer;
+
+    //current hours and day
+    private float TimeOfDayMinutes;
+    private int TimeOfDayHours;
+
+    //which hour of day the sun rises and sets
+    private int TimeOfSunrise = 8;
+    private int TimeOfSunset = 20;
+    
+
+    //the regular speed of the day + clock. not to be confused with time manipulation
+    private int timeSpeed = 90;
+
 
     void Start()
     {
         mainLight = this.gameObject.GetComponent<Light>();
-
-        setTimeDay();
+        setTimeNight();
+        transitionTimer = 1f;
     }
 
     void Update()
     {
-        TransitionTimer += Time.deltaTime;
-        mainLight.color = Color.Lerp(currentTimeColor, targetTimeColor, TransitionTimer);
+        transitionTimer += Time.deltaTime;
 
+        UpdateClock();
+        
+        if (transitionTimer < 1)
+            mainLight.color = Color.Lerp(currentTimeColor, targetTimeColor, transitionTimer);
+        
+    }
 
-        if (Input.GetButtonDown("Fire1"))
+    private void UpdateClock()
+    {
+        transitionTimer += Time.deltaTime;
+        TimeOfDayMinutes += Time.deltaTime * timeSpeed;
+
+        timeText.text = $"{TimeOfDayHours}:{Mathf.Floor(TimeOfDayMinutes)}";
+
+        if (TimeOfDayMinutes > 60)
         {
-            if (isDay)
-            {
-                setTimeNight();
-            }
-            else
+            TimeOfDayMinutes = 0;
+            TimeOfDayHours += 1;
+
+            //check if its morning, nighttime or midnight
+            if (TimeOfDayHours == TimeOfSunrise)
             {
                 setTimeDay();
+            } else if (TimeOfDayHours == TimeOfSunset)
+            {
+                setTimeNight();
+            } else if (TimeOfDayHours == 24)
+            {
+                TimeOfDayHours = 0;
             }
-            
         }
     }
 
-    public void setTimeDay()
+    private void setTimeDay()
     {
-        currentTimeColor = dayTimeColor;
-        targetTimeColor = nightTimeColor;
-
-        isDay = true;
-        TransitionTimer = 0f;
-    }
-
-    public void setTimeNight()
-    {
+        //from nighttime to daytime
         currentTimeColor = nightTimeColor;
         targetTimeColor = dayTimeColor;
 
+        isDay = true;
+        transitionTimer = 0f;
+    }
+
+    private void setTimeNight()
+    {
+        //from daytime to nighttime
+        currentTimeColor = dayTimeColor;
+        targetTimeColor = nightTimeColor;
+
         isDay = false;
-        TransitionTimer = 0f;
+        transitionTimer = 0f;
     }
 }
