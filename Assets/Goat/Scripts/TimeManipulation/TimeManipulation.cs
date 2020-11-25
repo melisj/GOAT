b73667a9f70f1f0c3f7e4944eaec8162fd4e6573
boxheadt <Timohondema@hotmail.com>
2020-11-25 15:27:33 +0100

    Time UI
    Added - Time Ui and Prefabs

--- Assets/Goat/Scripts/TimeManipulation/TimeManipulation.cs
+++ Assets/Goat/Scripts/TimeManipulation/TimeManipulation.cs
@@ -6,6 +6,7 @@ public class TimeManipulation : MonoBehaviour
 {
     public float scaleOfTime = 1f;
     public float pausetime = 0f;
+    public float playTime = 1f;    
     public float forwardTime = 10f;
     public float fastForwardTime = 15f;
 
@@ -20,13 +21,13 @@ public class TimeManipulation : MonoBehaviour
     }
    public void Play()
     {
-        scaleOfTime = scaleOfTime;
+        scaleOfTime = playTime;
     }
     public void Forward()
     {
         scaleOfTime = forwardTime;
     }
-    public void FastforwardForward()
+    public void FastForward()
     {
         scaleOfTime = fastForwardTime;
     }
