5e3e3fac58e91a9fc9c6306afd8c4af30a083b97
jasperbarg <jaspervandenbarg@hotmail.nl>
2020-12-01 17:01:11 +0100

    Started on StateMachine.

---
+++ Assets/Goat/Scripts/AI/IState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Goat.AI
+{
+    public interface IState
+    {
+        void Tick();
+
+        void OnEnter();
+
+        void OnExit();
+    }
+}
+
