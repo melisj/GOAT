d632a44663de8ce380619a4718b8bec341e0c71e
jasperbarg <jaspervandenbarg@hotmail.nl>
2020-12-02 13:03:25 +0100

    Started working on Customer to use state machine.

---
+++ Assets/Goat/Scripts/AI/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Goat.AI.States;
+using Goat.Storage;
+using Goat.Grid.Interactions;
+
+namespace Goat.AI
+{
+    public class Customer : MonoBehaviour
+    {
+        private StateMachine stateMachine;
+
+        private StorageInteractable targetStorage;
+
+        void Awake()
+        {
+            //code
+            var navMeshAgent = GetComponent<NavMeshAgent>();
+            //var animator = GetComponent<Animator>();
+            stateMachine = new StateMachine();
+
+            // States
+
+            // Enter store
+            // Setup shopping list
+            // Search for targets
+            // Move to target
+            var moveToTarget = new MoveToTarget(gameObject.transform, Vector3.zero, navMeshAgent, new Animator());
+            // Take from target
+            // Go to checkout
+            // Pay
+            // Exit store
+
+            // Transitions
+
+
+            // Functions
+
+            Func<bool> StuckForSeconds() => () => moveToTarget.timeStuck > 1f;
+            Func<bool> HasTarget() => () => targetStorage != null;
+        }
+    }
+}
+
---
+++ Assets/Goat/Scripts/AI/States/MoveToTarget.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Goat.AI.States
+{
+    public class MoveToTarget : IState
+    {
+        private Transform agent;
+        private Vector3 target;
+        private NavMeshAgent navMeshAgent;
+        private Animator animator;
+
+        private Vector3 lastLocation;
+        public float timeStuck;
+
+
+        public MoveToTarget(Transform agent, Vector3 target, NavMeshAgent navMeshAgent, Animator animator)
+        {
+            this.agent = agent;
+            this.target = target;
+            this.navMeshAgent = navMeshAgent;
+            //this.animator = animator;
+        }
+
+        public void Tick()
+        {
+            // Check if agent is stuck while navigating to target
+            if (Vector3.Distance(agent.position, lastLocation) <= 0)
+                timeStuck += Time.deltaTime;
+
+            lastLocation = agent.position;
+        }
+
+        public void OnEnter()
+        {
+            timeStuck = 0f;
+            navMeshAgent.enabled = true;
+            navMeshAgent.SetDestination(target);
+            // Animation
+        }
+
+        public void OnExit()
+        {
+            navMeshAgent.enabled = false;
+            // Animation
+        }
+    }
+}
+
