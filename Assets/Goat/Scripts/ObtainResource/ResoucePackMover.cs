01158f5d144348e4f4785eb172bb5931a08530c8
Stanley <stanley.ursem@hotmail.com>
2020-12-08 12:08:42 +0100

    Added the pipe data assets

---
+++ Assets/Goat/Scripts/ObtainResource/ResoucePackMover.cs
@@ -0,0 +1,17 @@
+﻿using Goat.CameraControls;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ResoucePackMover : MovementSystem
+{
+    [SerializeField] private Vector3[] tubePoints;
+    [SerializeField] private float durationPerTube;
+
+    public void Setup(Vector3[] wayPoints)
+    {
+        tubePoints = wayPoints;
+        transform.DOPath(tubePoints, durationPerTube * tubePoints.Length);
+    }
+}
---
+++ Assets/Goat/Scripts/ObtainResource/TubeDirection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Goat.Farming
+{
+    public class TubeDirection : MonoBehaviour
+    {
+        [SerializeField] private FarmStationFunction connectedFarm;
+        public FarmStationFunction ConnectedFarm { get => connectedFarm; set => connectedFarm = value; }
+    }
+}
