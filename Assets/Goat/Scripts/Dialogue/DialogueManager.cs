5b4d1cc450866b64de3df1085efbbc08a21e257e
boxheadt <Timohondema@hotmail.com>
2020-12-08 12:23:48 +0100

    Random names
    Added - random names for customers.

--- Assets/Goat/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Goat/Scripts/Dialogue/DialogueManager.cs
@@ -6,41 +6,59 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
     public Text nameText;
-    public Text dialogueText;
-
+    public Text dialogueText;  
     public Animator animator;
+    public List<string> firstNames = new List<string>();
+    public List<string> lastNames = new List<string>();
 
-    private Queue<string> sentences;
-
+    int NameNumber;
+    int listMax;
+    
     void Start()
     {
-        sentences = new Queue<string>();
-    }
+        firstNames.Clear();
+        firstNames.Add("Grogu");
+        firstNames.Add("Shifu");
+        firstNames.Add("Koli");
 
+        lastNames.Clear();
+        lastNames.Add("Hopsnop");
+        lastNames.Add("Fropstop");
+        lastNames.Add("Gorbluk");
+        lastNames.Add("Druppok");
+
+    }
+    void Update()
+    {
+        if (firstNames.Count < lastNames.Count)
+        {
+            listMax = lastNames.Count + 1;
+        }
+        else
+            listMax = firstNames.Count + 1;
+
+       NameNumber = Random.Range(0, listMax);
+      
+    }
     public void StartDialogue(Dialogue dialogue)
     {
         animator.SetBool("IsOpen", true);
-        nameText.text = dialogue.name;
 
-        sentences.Clear();
-
-       foreach (string sentence in dialogue.sentence)
-        {
-            sentences.Enqueue(sentence);
+        for (int i = 0; i < NameNumber; i++)
+        {           
+                nameText.text = firstNames[i] + " " + lastNames[i];            
         }
-        DisplayNextSentence();
+
+        
     }
 
     public void DisplayNextSentence()
     {
-        if (sentences.Count == 0)
-        {
-            EndDialogue();
-            return;
-        }
-
-        string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        
+        dialogueText.text = "Test";
+      
+      
+        
     }
 
     void EndDialogue()
