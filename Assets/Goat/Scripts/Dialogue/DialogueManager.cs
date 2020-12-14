8db2602843930913c787d59c69d648e754546086
boxheadt <Timohondema@hotmail.com>
2020-12-14 12:06:50 +0100

    added fix
    Fixed code convenstion

--- Assets/Goat/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Goat/Scripts/Dialogue/DialogueManager.cs
@@ -6,153 +6,158 @@ using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    public Text nameText;
-    public Text dialogueOneText;
-    public Text dialogueTwoText;
-    public Text dialogueThreeText;
-    public Animator animator;
-    public NPC npc;
-    string inventoryList;
+	public Text nameText;
+	public Text dialogueOneText;
+	public Text dialogueTwoText;
+	public Text dialogueThreeText;
+	public Animator animator;
+	public NPC npc;
+	string inventoryList;
 
-    public List<string> firstNames = new List<string>();
-    public List<string> myDialogueOne = new List<string>();
-    public List<string> myDialogueTwo = new List<string>();
-    public List<string> myDialogueThree = new List<string>();
-    public List<string> lastNames = new List<string>();
-    public List<string> sentenceOne = new List<string>();
-    public List<string> sentenceTwo = new List<string>();
-    public List<string> sentenceThree = new List<string>();
+	public List<string> firstNames = new List<string>();
+	public List<string> myDialogueOne = new List<string>();
+	public List<string> myDialogueTwo = new List<string>();
+	public List<string> myDialogueThree = new List<string>();
+	public List<string> lastNames = new List<string>();
+	public List<string> sentenceOne = new List<string>();
+	public List<string> sentenceTwo = new List<string>();
+	public List<string> sentenceThree = new List<string>();
 
-    int nameNumber;
-    int dialogueNumber;
-    int listNameMax;
-    int listDialogueMax;
+	int nameNumber;
+	int dialogueNumber;
+	int listNameMax;
+	int listDialogueMax;
 
-    bool senOneDone = false;
-    bool senTwoDone = false;
-    bool senThreeDone = false;
+	bool senOneDone = false;
+	bool senTwoDone = false;
+	bool senThreeDone = false;
 
-    void Start()
-    {
+	void Start()
+	{
+		// First names
+		firstNames.Clear();
+		firstNames.Add("Grogu");
+		firstNames.Add("Shifu");
+		firstNames.Add("Koli");
 
-        inventoryList += " " + npc.inventory.Keys.ToString() + " " + npc.inventory.Values;
+		// Last names
+		lastNames.Clear();
+		lastNames.Add("Hopsnop");
+		lastNames.Add("Fropstop");
+		lastNames.Add("Gorbluk");
+		lastNames.Add("Druppok");
 
-        // First names
-        firstNames.Clear();
-        firstNames.Add("Grogu");
-        firstNames.Add("Shifu");
-        firstNames.Add("Koli");
+		// The first sentences 
+		sentenceOne.Clear();
+		sentenceOne.Add("Hello,");
+		sentenceOne.Add("Good day,");
+		sentenceOne.Add("HELLO, I WAS FIRST");
 
-        // Last names
-        lastNames.Clear();
-        lastNames.Add("Hopsnop");
-        lastNames.Add("Fropstop");
-        lastNames.Add("Gorbluk");
-        lastNames.Add("Druppok");
+		// The second sentences
+		sentenceTwo.Clear();
+		sentenceTwo.Add("I want to pay for");
+		sentenceTwo.Add("How much is this");
+		sentenceTwo.Add("YES,thank you I want to pay");
 
-        // The first sentences 
-        sentenceOne.Clear();
-        sentenceOne.Add("Hello,");
-        sentenceOne.Add("Good day,");
-        sentenceOne.Add("HELLO, I WAS FIRST");
-
-        // The second sentences
-        sentenceTwo.Clear();
-        sentenceTwo.Add("I want to pay for");
-        sentenceTwo.Add("How much is this");
-        sentenceTwo.Add("YES,thank you I want to pay");
-
-        // The third sentences
-        sentenceThree.Clear();
-        sentenceThree.Add("Goodbye");
-        sentenceThree.Add("Farewell");
-        sentenceThree.Add("Bye");
+		// The third sentences
+		sentenceThree.Clear();
+		sentenceThree.Add("Goodbye");
+		sentenceThree.Add("Farewell");
+		sentenceThree.Add("Bye");
 
 
-        myDialogueOne.Clear();
-        myDialogueTwo.Clear();
-        myDialogueThree.Clear();
-    }
-     void Update()
-    {
-        nameNumber = Random.Range(0, listNameMax);
-        dialogueNumber = Random.Range(0, listDialogueMax);
-    }
-    public void StartDialogue(Dialogue dialogue)
-    {
-        animator.SetBool("IsOpen", true);
+		myDialogueOne.Clear();
+		myDialogueTwo.Clear();
+		myDialogueThree.Clear();
+	}
+	void Update()
+	{
+		nameNumber = Random.Range(0, listNameMax);
+		dialogueNumber = Random.Range(0, listDialogueMax);
+	}
+	public void StartDialogue(Dialogue dialogue)
+	{
+		animator.SetBool("IsOpen", true);
 
-        //check wich list is longest a use that one as max
-        if (sentenceOne.Count > sentenceTwo.Count || sentenceOne.Count > sentenceThree.Count)
-        {
-            listDialogueMax = sentenceOne.Count + 1;
-        }
-        if (sentenceTwo.Count > sentenceOne.Count || sentenceTwo.Count > sentenceThree.Count)
-        {
-            listDialogueMax = sentenceTwo.Count + 1;
-        }
-        else
-        {
-            listDialogueMax = sentenceThree.Count + 1;
-        }
+		//check wich list is longest a use that one as max
+		if (sentenceOne.Count > sentenceTwo.Count || sentenceOne.Count > sentenceThree.Count)
+		{
+			listDialogueMax = sentenceOne.Count + 1;
+		}
+		if (sentenceTwo.Count > sentenceOne.Count || sentenceTwo.Count > sentenceThree.Count)
+		{
+			listDialogueMax = sentenceTwo.Count + 1;
+		}
+		else
+		{
+			listDialogueMax = sentenceThree.Count + 1;
+		}
 
-        //check which list is longer and use that one as the max
-        if (firstNames.Count < lastNames.Count)
-        {
-            listNameMax = lastNames.Count;
-            
-        }
-        else
-        {
-            listNameMax = firstNames.Count;
-        }
+		//check which list is longer and use that one as the max
+		if (firstNames.Count < lastNames.Count)
+		{
+			listNameMax = lastNames.Count;
 
-        //display the name
-        for (int i = 0; i < nameNumber; i++)
-        {           
-                nameText.text = firstNames[i] + " " + lastNames[i];            
-        }
+		}
+		else
+		{
+			listNameMax = firstNames.Count;
+		}
 
-    }
-    public void DisplayNextSentence()
-    {
-        animator.SetBool("IsOpen", true);
-    }
+		//display the name
+		for (int i = 0; i < nameNumber; i++)
+		{
+			nameText.text = firstNames[i] + " " + lastNames[i];
+		}
 
-    void EndDialogue()
-    {
-        animator.SetBool("IsOpen", false);
+	}
+	public void DisplayNextSentence()
+	{
+		animator.SetBool("IsOpen", true);
+	}
 
-    }
+	void EndDialogue()
+	{
+		animator.SetBool("IsOpen", false);
 
-   public void DialogueOne()
-    {
-        senOneDone = true;
-        //display the sentences in the porper order and with the right randomness
-        for (int i = 0; i < dialogueNumber; i++)
-        {
-            if (senOneDone == true)
-            {
-                dialogueOneText.text = sentenceOne[i];
-            }
-            
-        }
-    }
+	}
 
-  public  void DialogueTwo()
-    {
-        senTwoDone = true;
-        for (int i = 0; i < dialogueNumber; i++)
-        {
-            dialogueTwoText.text = sentenceTwo[i] + inventoryList;
-        }
-    }
-    public void DialogueThree()
-    {
-        for (int i = 0; i < dialogueNumber; i++)
-        {
-            dialogueThreeText.text = sentenceThree[i];
-            senThreeDone = true;
-        }
-    }
+	public void DialogueOne()
+	{
+		senOneDone = true;
+		//display the sentences in the porper order and with the right randomness
+		for (int i = 0; i < dialogueNumber; i++)
+		{
+			if (senOneDone == true)
+			{
+				dialogueOneText.text = sentenceOne[i];
+			}
+
+		}
+	}
+
+	public void DialogueTwo()
+	{
+		senTwoDone = true;
+		for (int i = 0; i < dialogueNumber; i++)
+		{
+			dialogueTwoText.text = sentenceTwo[i] + inventoryList;
+		}
+	}
+
+	private string SomeInventory(NPC customer)
+	{
+
+		string invi = "";
+	
+		return invi ;
+	}
+	public void DialogueThree()
+	{
+		for (int i = 0; i < dialogueNumber; i++)
+		{
+			dialogueThreeText.text = sentenceThree[i];
+			senThreeDone = true;
+		}
+	}
 }
