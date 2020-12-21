075e379889d5c631b737bcaeadf402309c69a5a3
Stanley <stanley.ursem@hotmail.com>
2020-12-21 11:06:04 +0100

    Initial push

---
+++ Assets/Goat/_Scripts/Audio/Audio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Goat.Events;
+
+namespace Goat.Audio
+{
+    public class Audio : IPairable<AudioCue, AudioConfiguration>
+    {
+        [SerializeField] private AudioCue cue;
+        [SerializeField] private AudioConfiguration audioConfiguration;
+
+        public AudioCue Item1 { get => cue; set => cue = value; }
+        public AudioConfiguration Item2 { get => audioConfiguration; set => audioConfiguration = value; }
+
+        public void Deconstruct(out AudioCue cue, out AudioConfiguration audioConfiguration)
+        {
+            cue = Item1;
+            audioConfiguration = Item2;
+        }
+    }
+}
---
+++ Assets/Goat/_Scripts/Audio/AudioEventChannel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityAtoms;
+
+namespace Goat.Audio
+{
+    [EditorIcon("atom-icon-cherry")]
+    [CreateAssetMenu(menuName = "Unity Atoms/Events/AudioEventChannel", fileName = "AudioEventChannel")]
+    public class AudioEventChannel : AtomEvent<Audio>
+    {
+    }
+}
