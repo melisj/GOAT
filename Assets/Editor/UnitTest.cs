80ea05e8367836d591f2eacfd94f5743c0d97565
johan han <Methaan123@gmail.com>
2021-01-03 15:21:09 +0100

    Added CI/CD setup

---
+++ Assets/Editor/UnitTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitTest 
+{
+    [Test]
+    public void TestingFunction()
+    {
+        Assert.Pass();
+    }
+}
