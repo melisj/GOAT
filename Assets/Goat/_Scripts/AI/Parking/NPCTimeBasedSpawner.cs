8d37a7beba6f76cfc5d7843253385f1bbc9b865c
johan han <Methaan123@gmail.com>
2021-01-04 16:05:40 +0100

    Setup for spawning npc's

---
+++ Assets/Goat/_Scripts/AI/Parking/NPCTimeBasedSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+using Goat.Events;
+
+namespace Goat.AI.Parking
+{
+    public class NPCTimeBasedSpawner : EventListenerBool
+    {
+        [SerializeField] private Vector2 spawnTimeRange;
+        [SerializeField] private ShipSpawner spawner;
+
+        private bool day;
+        private Coroutine spawnRoutine;
+
+        public override void OnEventRaised(bool day)
+        {
+            this.day = day;
+        }
+
+        public void Update()
+        {
+            if (day)
+            {
+                if (spawnRoutine == null)
+                    spawnRoutine = StartCoroutine(SpawnNpc());
+            }
+        }
+
+        public IEnumerator SpawnNpc()
+        {
+            float spawnTime = Random.Range(spawnTimeRange.x, spawnTimeRange.y);
+            float time = 0;
+
+            while (time < spawnTime)
+            {
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            spawner.SpawnShip(1);
+            spawnRoutine = null;
+        }
+    }
+}
