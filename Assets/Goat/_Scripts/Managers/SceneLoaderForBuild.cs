bd31133475e9df3dcd4a4ba30773c92e573b3d66
johan han <Methaan123@gmail.com>
2021-01-05 14:24:58 +0100

    Added sceneloader for build and editor

---
+++ Assets/Goat/_Scripts/Managers/SceneLoaderForBuild.cs
@@ -0,0 +1,34 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderForBuild : MonoBehaviour
+{
+    [SerializeField, ReadOnly] private string[] scenePaths;
+
+    public void OnEnable()
+    {
+        LoadAllScenes();
+    }
+
+    public void SetPaths(string[] scenePaths)
+    {
+        this.scenePaths = scenePaths;
+    }
+
+    public void LoadAllScenes()
+    {
+        foreach(string path in scenePaths) 
+        {
+            SceneManager.LoadScene(path, LoadSceneMode.Additive);
+        }
+    }
+
+    [Button("Empty list")]
+    public void EmptyList()
+    {
+        scenePaths = null;
+    }
+}
---
+++ Assets/Goat/_Scripts/Managers/SceneLoaderForEditor.cs
@@ -0,0 +1,59 @@
+﻿#if UNITY_EDITOR
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(SceneLoaderForBuild))]
+public class SceneLoaderForEditor : MonoBehaviour
+{
+    [SerializeField, ReadOnly]private SceneAsset[] scencesToLoad;
+    
+    private SceneLoaderForBuild sceneLoaderBuild;
+    public SceneLoaderForBuild SceneLoaderBuild { get {
+            if (sceneLoaderBuild == null)
+                sceneLoaderBuild = GetComponent<SceneLoaderForBuild>();
+            return sceneLoaderBuild;
+        }
+    }
+
+    [Button("Load necessary scenes")]
+    public void LoadScenesInEditor()
+    {
+        if (EditorUtility.DisplayDialog("Load Scenes", "This will load in new scenes listed in the inspector!", "Load all", "Nope!"))
+        {
+            scencesToLoad = Resources.LoadAll<SceneAsset>("SceneLoader");
+            LoadAllScenes();
+        }
+    }
+
+    [Button("Store paths for build")]
+    public void StoreScenesForBuild()
+    {
+        scencesToLoad = Resources.LoadAll<SceneAsset>("SceneLoader");
+        StoreScenes();
+    }
+
+    private void StoreScenes()
+    {
+        string[] scenePaths = new string[scencesToLoad.Length];
+        for(int i = 0; i < scencesToLoad.Length; i++) 
+        {
+            scenePaths[i] = AssetDatabase.GetAssetPath(scencesToLoad[i]);
+        }
+        SceneLoaderBuild.SetPaths(scenePaths);
+    }
+
+    private void LoadAllScenes()
+    {
+        foreach (SceneAsset scene in scencesToLoad)
+        {
+            if (Application.isPlaying)
+                SceneManager.LoadScene(AssetDatabase.GetAssetPath(scene), LoadSceneMode.Additive);
+            else
+                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(scene), OpenSceneMode.Additive);
+        }
+    }
+}
+#endif
