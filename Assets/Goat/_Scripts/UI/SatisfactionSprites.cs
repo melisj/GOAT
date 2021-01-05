ba7bfca19d96a63aed611f4da7fc31ca1c7d21d9
Stanley <stanley.ursem@hotmail.com>
2021-01-05 22:09:25 +0100

    Added UI atlas, disabled scrollbars, fixed some pixelated UI windows

---
+++ Assets/Goat/_Scripts/UI/SatisfactionSprites.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SatisfactionSprites", menuName = "ScriptableObjects/GlobalVariables/SatisfactionSprites")]
+public class SatisfactionSprites : ScriptableObject
+{
+    [SerializeField] private Sprite happy;
+    [SerializeField] private Sprite unhappy;
+    [SerializeField] private Sprite neutral;
+
+    public Sprite Happy => happy;
+    public Sprite UnHappy => unhappy;
+    public Sprite Neutral => neutral;
+}
