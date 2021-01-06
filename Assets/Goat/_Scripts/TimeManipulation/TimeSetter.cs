56de74fc22505a3be6b7d2338805383d55e2654c
Stanley <stanley.ursem@hotmail.com>
2021-01-06 10:33:52 +0100

    Fixed bug where you could still build at day Added time buttons switching sprites and being able to set time speed aswell as timescale

--- Assets/Goat/_Scripts/TimeManipulation/TimeSetter.cs
+++ Assets/Goat/_Scripts/TimeManipulation/TimeSetter.cs
@@ -1,20 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimeSetter : MonoBehaviour
+public class TimeSetter : MonoBehaviour, IAtomListener<UnityAtoms.Void>
 {
     [SerializeField] private Button timeButton;
-    [SerializeField] private int timeSet;
+    [SerializeField] private int timeScaleSet;
+    [SerializeField] private int timeSpeedSet;
+    [SerializeField] private IntEvent onTimeSpeedChanged;
+    [SerializeField] private VoidEvent onTimeButtonClicked;
+    [SerializeField] private Image timeIcon;
+    [SerializeField] private Sprite activatedSprite, deactivatedSprite;
+    [SerializeField] private bool active;
+
+    public void OnEventRaised(Void item)
+    {
+        ChangeIcon();
+    }
+
+    private void ChangeIcon()
+    {
+        timeIcon.sprite = active ? activatedSprite : deactivatedSprite;
+        active = !active && active;
+    }
 
     private void Awake()
     {
         timeButton.onClick.AddListener(SetTime);
+        ChangeIcon();
+    }
+
+    private void OnEnable()
+    {
+        onTimeButtonClicked.RegisterSafe(this);
+    }
+
+    private void OnDisable()
+    {
+        onTimeButtonClicked.UnregisterSafe(this);
     }
 
     private void SetTime()
     {
-        Time.timeScale = timeSet;
+        onTimeButtonClicked.Raise();
+        Time.timeScale = timeScaleSet;
+        onTimeSpeedChanged.Raise(timeSpeedSet);
+        active = true;
+        ChangeIcon();
     }
 }
