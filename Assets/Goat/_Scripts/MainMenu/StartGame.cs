c280284488d4995cbb55b3d655be8b3c950a6cc4
johan han <Methaan123@gmail.com>
2021-01-06 11:03:41 +0100

    Fixed loading requiring the main menu to be loaded

---
+++ Assets/Goat/_Scripts/MainMenu/StartGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private SceneLoaderForBuild sceneLoader;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(LoadGame);
+    }
+
+    private void LoadGame()
+    {
+        StartCoroutine(sceneLoader.LoadAllScenes(LoadComplete));
+    }
+
+    private void LoadComplete()
+    {
+        // Unload the first build scene
+        SceneManager.UnloadSceneAsync(0);
+    }
+}
