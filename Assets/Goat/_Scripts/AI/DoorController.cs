b9e3d3ce49cfd195f5f96b1fcb7cfbea137ae66e
jasperbarg <jaspervandenbarg@hotmail.nl>
2021-01-06 12:54:25 +0100

    Worked on door behaviour. Door should play sounds but didnt work in my test scene, maybe it will work in a full build scene because my test scene is missing pool manager and such.

---
+++ Assets/Goat/_Scripts/AI/DoorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Animator doorAnimator;
+    [SerializeField] private bool ring = false;
+    [SerializeField] private DoorRingSound doorRing;
+    [SerializeField] private DoorOpenCloseSound doorOpenClose;
+
+    [SerializeField] private int agentsInCollider = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(agentsInCollider == 0)
+        {
+            doorRing.PlayAudio();
+            doorOpenClose.PlayAudio();
+        }
+
+        agentsInCollider++;
+        //doorAnimator.SetInteger("AgentsInCollider", agentsInCollider);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        agentsInCollider--;
+        //doorAnimator.SetInteger("AgentsInCollider", agentsInCollider);
+
+        if (agentsInCollider == 0)
+        {
+            doorRing.PlayAudio();
+            doorOpenClose.PlayAudio();
+        }
+    }
+}
