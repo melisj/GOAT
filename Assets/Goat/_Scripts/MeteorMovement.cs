0bfd4909df28906a8e9b0dd09742ae9867bf2aa0
Joppe Min <joppemin@hotmail.com>
2021-01-08 16:33:10 +0100

    Meteor, meteor impact and meteor movement script.

---
+++ Assets/Goat/_Scripts/MeteorMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject explosion;
+    void Update()
+    {
+       this.transform.position -= transform.up /10;
+        Explode();
+    }
+
+    void Explode()
+    {
+        if (transform.position.y < -3)
+        {
+            Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+        //doe particle
+        //replace tiles met resources
+    }
+}
