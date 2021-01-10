8002c7f1631f4a93b6cae58bed1212f21fa3be8a
Joppe Min <joppemin@hotmail.com>
2021-01-10 21:02:23 +0100

    Meteor Impact gets destroyed

---
+++ Assets/Goat/_Scripts/MeteorDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorDestroy : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(DestroyThis());
+    }
+
+    IEnumerator DestroyThis()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
+}
