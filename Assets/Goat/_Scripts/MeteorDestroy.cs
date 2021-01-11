fb8005783f09e20ae865da76d0bb4625412981d6
Stanley <stanley.ursem@hotmail.com>
2021-01-11 11:31:59 +0100

    Camera shake on meteor impact

--- Assets/Goat/_Scripts/MeteorDestroy.cs
+++ Assets/Goat/_Scripts/MeteorDestroy.cs
@@ -1,6 +1,7 @@
 ﻿using Goat.Pooling;
 using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
 public class MeteorDestroy : MonoBehaviour, IPoolObject
@@ -8,11 +9,13 @@ public class MeteorDestroy : MonoBehaviour, IPoolObject
     public int PoolKey { get; set; }
     public ObjectInstance ObjInstance { get; set; }
     [SerializeField] private AudioCue cue;
+    [SerializeField] private VoidEvent onMeteorImpact;
 
     public void OnGetObject(ObjectInstance objectInstance, int poolKey)
     {
         ObjInstance = objectInstance;
         PoolKey = poolKey;
+        onMeteorImpact.Raise();
         cue.PlayAudioCue(() => PoolManager.Instance.ReturnToPool(gameObject));
     }
 
